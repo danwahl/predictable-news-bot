f18ad3dc9a3f73c2cc0653385350474007134ed1
Dan Wahl <drwahl@gmail.com>
2022-08-09 00:41:14 -0500

    Using searchQuestions instead of frontpage for query, creating get_cl… (#7)
    Using searchQuestions instead of frontpage for query, creating get_client functions, additional cleanup

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ import argparse
 import logging
 import os
 import pandas as pd
+import sys
 import tweepy
 
 # Default probability difference threshold, above which a tweet is sent
 DIFF_THRESHOLD = 0.05
 
-# GraphQL query to get the history of all frontpage current questions
+# GraphQL query to get the history of all 4+ star rated questions (using space as pseudo-wildcard)
 QUERY = gql("""
 {
-  frontpage {
+  searchQuestions(input: {query: " ", starsThreshold: 4, limit: 1000}) {
     history {
       fetched
       options {
@@ -33,6 +34,42 @@ QUERY = gql("""
 TWEET_LENGTH = 280
 
 
+def get_gql_client():
+    try:
+        # Set GraphQL logging level (defaults to INFO otherwise)
+        log.setLevel(logging.WARNING)
+
+        # Create GraphQL transport using requests
+        # https://gql.readthedocs.io/en/latest/transports/requests.html
+        transport = RequestsHTTPTransport(
+            url="https://metaforecast.org/api/graphql")
+
+        # Create GraphQL client
+        return Client(transport=transport,
+                      fetch_schema_from_transport=True)
+    except Exception as e:
+        logging.error(f"Failed to fetch data: {e}")
+        return None
+
+
+def get_tweepy_client():
+    try:
+        # Load environment variables from .env file
+        load_dotenv()
+
+        # Create Twitter client
+        # https://docs.tweepy.org/en/stable/authentication.html#id3
+        return tweepy.Client(
+            consumer_key=os.getenv("CONSUMER_KEY"),
+            consumer_secret=os.getenv("CONSUMER_SECRET"),
+            access_token=os.getenv("ACCESS_TOKEN"),
+            access_token_secret=os.getenv("ACCESS_TOKEN_SECRET")
+        )
+    except Exception as e:
+        logging.error(f"Failed to login: {e}")
+        return None
+
+
 def main():
     # Configure logging
     logging.basicConfig(level=logging.INFO)
@@ -47,55 +84,42 @@ def main():
     args = parser.parse_args()
     logging.debug(f"Args: {args}")
 
+    tweepy_client = get_tweepy_client()
+    if tweepy_client is None:
+        sys.exit(1)
+
     try:
-        # Load environment variables from .env file
-        load_dotenv()
-
-        # Create Twitter client
-        # https://docs.tweepy.org/en/stable/authentication.html#id3
-        client = tweepy.Client(
-            consumer_key=os.getenv("CONSUMER_KEY"),
-            consumer_secret=os.getenv("CONSUMER_SECRET"),
-            access_token=os.getenv("ACCESS_TOKEN"),
-            access_token_secret=os.getenv("ACCESS_TOKEN_SECRET")
-        )
-
         # Log Twitter username
-        me = client.get_me()
+        me = tweepy_client.get_me()
         username = me.data["username"]
         logging.debug(f"Logged in as {username}")
     except Exception as e:
-        logging.error(f"Failed to login: {e}")
-        return
+        logging.error(f"Failed to get username: {e}")
+        sys.exit(1)
+
+    gql_client = get_gql_client()
+    if gql_client is None:
+        sys.exit(1)
 
     try:
-        # Set GraphQL logging level (defaults to INFO otherwise
-        log.setLevel(logging.WARNING)
-
-        # Create GraphQL transport using requests
-        # https://gql.readthedocs.io/en/latest/transports/requests.html
-        transport = RequestsHTTPTransport(
-            url="https://metaforecast.org/api/graphql")
-
-        # Create client and execute GraphQL query
-        result = Client(transport=transport,
-                        fetch_schema_from_transport=True).execute(QUERY)
-    except Exception as e:
-        logging.error(f"Failed to fetch data: {e}")
-        return
+        # Execute GraphQL query
+        result = gql_client.execute(QUERY)
+    except:
+        logging.error("Failed to execute query")
+        sys.exit(1)
 
     # Script expects to runs daily, so ignore data from before this datetime
     dt = datetime.now() - timedelta(hours=23)
 
-    # Iterate through all questions on frontpage
-    for i in range(len(result["frontpage"])):
-        question = result["frontpage"][i]
+    # Iterate through all questions
+    for i in range(len(result["searchQuestions"])):
+        question = result["searchQuestions"][i]
 
         # Get each probability history for all option for this question
-        history = pd.json_normalize(question["history"],
-                                    record_path=["options"], meta="fetched")
-        # >>> history.tail()
-        #        name  probability         fetched
+        df = pd.json_normalize(question["history"],
+                               record_path=["options"], meta="fetched")
+        # >>> df.tail()
+        #                name  probability         fetched
         # 58   38.0% to 38.2%     0.009615  1659699931.871
         # 59   38.3% to 38.5%     0.009615  1659699931.871
         # 60   38.6% to 38.8%     0.009615  1659699931.871
@@ -103,23 +127,8 @@ def main():
         # 62  39.2% or higher     0.894231  1659699931.871
 
         # Pivot around fetched timestamp (index) and options (columns)
-        history = history.pivot_table(index="fetched", columns="name",
-                                      values="probability")
-        # >>> history.tail()
-        # name          37.0% or lower  37.1% to 37.3%  37.4% to 37.6%  37.7% to 37.9%  38.0% to 38.2%  38.3% to 38.5%  38.6% to 38.8%  38.9% to 39.1%  39.2% or higher
-        # fetched
-        # 1.659354e+09        0.008850        0.017699        0.026549        0.044248        0.070796        0.088496        0.150442        0.212389         0.380531
-        # 1.659441e+09        0.009091        0.009091        0.009091        0.009091        0.045455        0.063636        0.136364        0.236364         0.481818
-        # 1.659527e+09        0.009009        0.009009        0.009009        0.009009        0.018018        0.063063        0.027027        0.180180         0.675676
-        # 1.659614e+09        0.009009        0.009009        0.009009        0.009009        0.009009        0.009009        0.090090        0.306306         0.549550
-        # 1.659700e+09        0.009615        0.009615        0.009615        0.009615        0.009615        0.009615        0.009615        0.038462         0.894231
-
-        # Make sure we have at least two rows so we can calculate the difference
-        if history.shape[0] < 2:
-            logging.warning(
-                f"Skipping question {question['id']} because it has only {history.shape[0]} row(s)")
-            continue
-
+        history = df.pivot_table(index="fetched", columns="name",
+                                 values="probability")
         # Convert index from fetched timestamp to datetime
         history.index = pd.to_datetime(history.index, unit="s")
         # >>> history.tail()
@@ -131,8 +140,13 @@ def main():
         # 2022-08-04 11:46:48.255000064        0.009009        0.009009        0.009009        0.009009        0.009009        0.009009        0.090090        0.306306         0.549550
         # 2022-08-05 11:45:31.871000064        0.009615        0.009615        0.009615        0.009615        0.009615        0.009615        0.009615        0.038462         0.894231
 
+        # Make sure we have at least two rows so we can calculate the difference
+        if history.shape[0] < 2:
+            logging.warning(
+                f"Skipping question {question['id']} because it has only {history.shape[0]} row(s)")
+            continue
         # Ignore this question if the most recent datetime is too old
-        if history.index[-1] < dt:
+        elif history.index[-1] < dt:
             logging.debug(
                 f"Skipping {question['id']} because {history.index[-1]} is earlier than {dt}")
             continue
@@ -152,8 +166,8 @@ def main():
         # 37.1% to 37.3%      0.009615  0.000606
         # 37.4% to 37.6%      0.009615  0.000606
 
-        # Get the option names that are above the difference threshold, if any
-        names = options.index[options["diff"].abs() > args.diff]
+        # Get the option names that are equal to or above the difference threshold, if any
+        names = options.index[options["diff"].abs() >= args.diff].values
         # Skip questions with no options above the threshold
         if len(names) == 0:
             logging.debug(
@@ -166,16 +180,11 @@ def main():
             text += f"\n- {name}: {100.0*options.loc[name, 'probability']:.1f}%"
             sign = "+" if options.loc[name, "diff"] > 0 else ""
             text += f" ({sign}{100.0*options.loc[name, 'diff']:.1f}%)"
-        # >>> text
-        # What will Joe Biden's RCP job approval rating be on Aug. 5?
-        # - 39.2% or higher: 89.4% (+34.5%)
-        # - 38.9% to 39.1%: 3.8% (-26.8%)
-        # - 38.6% to 38.8%: 1.0% (-8.0%)
 
         # Generate Metaforecast url (would use market url directly, but some are long)
         url = f"\nhttps://metaforecast.org/questions/{question['id']}"
 
-        # Append url to tweet text, trimming so that it fits (if necessary)
+        # Append url to tweet text, trimming as needed so that it fits
         if len(text) > (TWEET_LENGTH - len(url)):
             text = f"{text[:(TWEET_LENGTH - len(url) - 1)]}…"
         text += url
@@ -190,8 +199,8 @@ def main():
         if args.tweet:
             try:
                 logging.debug(f"Tweeting: {text}")
-                client.create_tweet(reply_settings="mentionedUsers",
-                                    text=text, user_auth=True)
+                tweepy_client.create_tweet(reply_settings="mentionedUsers",
+                                           text=text, user_auth=True)
                 sleep(1)
             except Exception as e:
                 logging.error(f"Failed to tweet: {e}")
